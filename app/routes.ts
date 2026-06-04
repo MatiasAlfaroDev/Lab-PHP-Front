c1a6558c2c9f498e6e5bf49ef02894b8bf2cd481
cecimenca <cecilia.mendez.c@estudiantes.utec.edu.uy>
2026-06-03 22:12:21 -0300

    Fix compra package route

--- app/routes.ts
+++ app/routes.ts
@@ -11,7 +11,7 @@ export default [
     route("professional/:id", "routes/client/professional.$id.tsx"),
     route("booking/:id/pay", "routes/client/booking.$id.pay.tsx"),
     route("package/:id/pay", "routes/client/package.$id.pay.tsx"),
-    route("compra-paquete/:id/pay", "routes/client/compra-package.$id.pay.tsx"),
+    route("compra-package/:id/pay", "routes/client/compra-package.$id.pay.tsx"),
     route("packages", "routes/client/packages.tsx"),
     route("messages", "routes/client/messages.tsx"),
     route("payments", "routes/client/payments.tsx"),
